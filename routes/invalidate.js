7f64217a0f58550820bec5128ccc72ec9c1a9b3d
Aakriti Singla <aakritis@seas.upenn.edu>
2015-04-26 21:33:22 -0400

    Invalidate Coupon Intial Commit

--- routes/invalidate.js
+++ routes/invalidate.js
@@ -1,4 +1,11 @@
 var newSession = require('client-sessions');
+var mysql      = require('mysql');
+var connection = {
+	host     : 'urbanbeats.czeuio4ikmlz.us-east-1.rds.amazonaws.com',
+	user     : 'urbanbeatsdb',
+	password : 'urbanbeatsdbpwd',
+	database : 'UrbanBeatsDB'
+}
 
 exports.do_work = function(request, res){
   res.render('invalidatecoupon.jade', {
